f09a2c34ef265df8a22b5bc01c0ab8feeae21852
Brandon Walsh <bmwalshy@gmail.com>
2024-02-18 01:27:01 -0500

    Trying my best

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Editor from './components/editor/Editor';
+import Menu from './components/menus/Menu';
 
 function App() {
   return (
     <div className="App">
+      <Menu />
       <Editor />
     </div>
   );
--- src/components/menus/Menu.js
+++ src/components/menus/Menu.js
@@ -1,8 +1,28 @@
 export default function Menu() {
+  const handleFileUpload = async (event) => {
+    const file = event.target.files[0];
+    
+    if (file) {
+      // Convert file to ArrayBuffer
+      const arrayBuffer = await file.arrayBuffer();
+
+      // Pass arrayBuffer to WebAssembly module
+      window._processFile(arrayBuffer);
+
+      console.log('done');
+    }
+  };
+
   return (
     <div>
       <ul>
-        <li>File</li>
+        <li>
+          File
+          <input
+            type="file"
+            onChange={handleFileUpload}
+          />
+        </li>
         <li>Data</li>
         <li>Edit</li>
         <li>View</li>
