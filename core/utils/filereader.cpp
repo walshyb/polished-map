c42d183de11cf276d215b489cc7266aa54352ea4
Brandon Walsh <bmwalshy@gmail.com>
2024-02-19 00:38:11 -0500

    remove junk

--- core/utils/filereader.cpp
+++ core/utils/filereader.cpp
@@ -31,29 +31,22 @@ void FileProcessor::processFile(const uint8_t* fileDataPtr, size_t bufferSize, c
  * @param filename Point to the name of the file
  */
 void FileProcessor::processAblk(const uint8_t* fileDataPtr, size_t bufferSize, const char* filename) {
-  // Process file data here
-  // Example: Printing the first 10 characters
-  std::cout << "File: " << filename << std::endl; 
-  std::cout << "File Size: " << bufferSize << std::endl; 
   int _width = 28;
   int _height = 18;
 
-  // open violetCity
   AppState *state = &AppState::getInstance();
 
+  // Loop through input
+  // Create block for each tile
   for (uint8_t y = 0; y < (size_t)_height; y++) {
     for (uint8_t x = 0; x < (size_t)_width; x++) {
       size_t i = (size_t)y * _width + (size_t)x;
       uint8_t id = fileDataPtr[i];
-      std::cout << "Index: " << i << ", val: " << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(id) << std::endl;
       // _blocks[i] = new Block(y, x, id);
-    }
-  }
 
-  for(size_t i = 0; i < bufferSize; i++) {
-    // Access buffer element
-   // uint8_t value = *(fileDataPtr + i);
-    //std::cout << value << std::endl;
+
+      // std::cout << "Index: " << i << ", val: " << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(id) << std::endl; // Print ID
+    }
   }
 }
 
