fd0db5667753fbdbdf6b252da246bc430f2517a6
Brandon Walsh <bmwalshy@gmail.com>
2024-02-18 19:09:05 -0500

    Parse albk file

--- core/utils/filereader.cpp
+++ core/utils/filereader.cpp
@@ -1,6 +1,9 @@
 #include <emscripten.h>
 #include <stdio.h>
 #include <iostream>
+#include <cstdio>
+#include <iomanip>
+
 
 
 extern "C" {
@@ -10,11 +13,23 @@ extern "C" {
     // Example: Printing the first 10 characters
     std::cout << "File: " << filename << std::endl; 
     std::cout << "File Size: " << bufferSize << std::endl; 
+    int _width = 28;
+    int _height = 18;
+    // open violetCity
+
+    for (uint8_t y = 0; y < (size_t)_height; y++) {
+      for (uint8_t x = 0; x < (size_t)_width; x++) {
+        size_t i = (size_t)y * _width + (size_t)x;
+        uint8_t id = fileDataPtr[i];
+        std::cout << "Index: " << i << ", val: " << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(id) << std::endl;
+        // _blocks[i] = new Block(y, x, id);
+      }
+    }
 
     for(size_t i = 0; i < bufferSize; i++) {
       // Access buffer element
-      uint8_t value = *(fileDataPtr + i);
-      std::cout << value << std::endl;
+     // uint8_t value = *(fileDataPtr + i);
+      //std::cout << value << std::endl;
     }
   }
 }
