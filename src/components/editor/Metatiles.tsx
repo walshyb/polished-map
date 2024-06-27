dcf82f6001e00c884aad195b6d39a4db4559272e
walshyb <bmwalshy@gmail.com>
2024-06-27 12:15:34 -0400

    Fix metatiles scroll in editor

--- src/components/editor/Metatiles.tsx
+++ src/components/editor/Metatiles.tsx
@@ -6,13 +6,14 @@ interface MetatilesProps {
 export default function Metatiles({ metatiles }: MetatilesProps) {
   return (
     <div className="metatiles">
-      {Object.keys(metatiles).map((key) => (
-        <div key={key}>
+      {Object.keys(metatiles).map((key, index) => (
+        <div className="metatile" key={key}>
           <img
             className="metatile"
             key={"metatile-" + key}
             src={`data:image/png;base64,${metatiles[key]}`}
           />
+          <span>{index}</span>
         </div>
       ))}
     </div>
